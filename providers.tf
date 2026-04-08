b76b697bc5b2dff32e5ad6a58dd0b844a3b79730
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-09 02:24:08 +0300

    update

--- providers.tf
+++ providers.tf
@@ -17,19 +17,4 @@ terraform {
   }
 }
 
-# ------------------------
-# AWS Provider
-# ------------------------
-provider "aws" {
-  region = var.aws_region
-
-  default_tags {
-    tags = {
-      Project     = var.project_name
-      Environment = var.environment
-      ManagedBy   = "Terraform"
-      Owner       = var.owner_email
-    }
-  }
-}
 
