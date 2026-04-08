41a2e034c9948ae7ddc1c131bcdd4c250a777cef
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-09 02:23:26 +0300

    update

--- providers.tf
+++ providers.tf
@@ -33,39 +33,3 @@ provider "aws" {
   }
 }
 
-# ------------------------
-# EKS Cluster Data Sources
-# ------------------------
-data "aws_eks_cluster" "cluster" {
-  name       = module.eks.cluster_name
-  depends_on = [module.eks]
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name       = module.eks.cluster_name
-  depends_on = [module.eks]
-}
-
-# ------------------------
-# Kubernetes Provider (EKS)
-# ------------------------
-provider "kubernetes" {
-  host = data.aws_eks_cluster.cluster.endpoint
-
-  cluster_ca_certificate = base64decode(
-    data.aws_eks_cluster.cluster.certificate_authority[0].data
-  )
-
-  # Recommended: avoids token expiration issues
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-
-    args = [
-      "eks",
-      "get-token",
-      "--cluster-name",
-      module.eks.cluster_name
-    ]
-  }
-}
