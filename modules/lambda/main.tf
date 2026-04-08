1c550038a7199e6ac20be425b558057ecdb5335e
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-09 02:28:00 +0300

    update

---
+++ modules/lambda/main.tf
@@ -0,0 +1,25 @@
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.function_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_lambda_function" "this" {
+  function_name = var.function_name
+  handler       = var.handler
+  runtime       = "nodejs18.x"
+  filename      = var.file_path
+  role          = aws_iam_role.lambda_role.arn
+
+  environment {
+    variables = var.environment
+  }
+}
