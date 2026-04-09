7127938f44d0c1c43c4cf9242156adffbfde8039
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-09 11:07:27 +0300

    update

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,3 @@
-output "queue_url" {
-  value = module.sqs.queue_url
-}
-
-output "topic_arn" {
-  value = module.sns.topic_arn
-}
-
-output "table_name" {
-  value = module.dynamodb.table_name
-}
 
 output "api_url" {
   value = module.api.api_url
