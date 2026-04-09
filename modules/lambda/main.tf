0aed8b685eba7dfbdd8064e24c0ddc0b4a0cc4c4
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-09 11:00:57 +0300

    update

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -23,3 +23,11 @@ resource "aws_lambda_function" "this" {
     variables = var.environment
   }
 }
+
+output "function_arn" {
+  value = aws_lambda_function.this.arn
+}
+
+output "function_name" {
+  value = aws_lambda_function.this.function_name
+}
