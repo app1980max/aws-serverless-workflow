f9f364abf1264285e1fe9595144052fb4b0c3254
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-09 10:58:40 +0300

    update

--- main.tf
+++ main.tf
@@ -1,24 +1,28 @@
+
 provider "aws" {
-  region = "us-west-2"
+  region = var.region
 }
 
 module "sqs" {
   source = "./modules/sqs"
+  name   = "orders-queue"
 }
 
 module "sns" {
   source = "./modules/sns"
+  name   = "orders-topic"
 }
 
 module "dynamodb" {
   source = "./modules/dynamodb"
+  table_name = "OrdersTable"
 }
 
 module "lambda_create" {
   source        = "./modules/lambda"
   function_name = "createOrder"
   handler       = "createOrder.handler"
-  file_path     = "../../functions/createOrder.zip"
+  file_path     = "./functions/createOrder.zip"
   environment = {
     QUEUE_URL = module.sqs.queue_url
   }
@@ -28,7 +32,7 @@ module "lambda_process" {
   source        = "./modules/lambda"
   function_name = "processOrder"
   handler       = "processOrder.handler"
-  file_path     = "../../functions/processOrder.zip"
+  file_path     = "./functions/processOrder.zip"
   environment = {
     TABLE_NAME = module.dynamodb.table_name
     TOPIC_ARN  = module.sns.topic_arn
@@ -39,5 +43,9 @@ module "lambda_notify" {
   source        = "./modules/lambda"
   function_name = "notifyUser"
   handler       = "notifyUser.handler"
-  file_path     = "../../functions/notifyUser.zip"
+  file_path     = "./functions/notifyUser.zip"
 }
+
+
+
+
